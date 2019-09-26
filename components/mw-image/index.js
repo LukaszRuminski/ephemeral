346452dcbf5b8a33df439ddc787b35a0cf22a188
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-09-26 14:23:37 +0200

    In progress

--- components/mw-image/index.js
+++ components/mw-image/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Image from '@mw/image'
 import { AbsolutePositionedChild, ImageWrapper, Layout, LayoutColumn } from './index.styles'
 
-const MWImagePage = ({ images }) => {
+const MWImageExample = ({ images }) => {
     return (
         <>
             <h2>Images</h2>
@@ -42,17 +42,8 @@ const MWImagePage = ({ images }) => {
     )
 }
 
-MWImagePage.defaultProps = {
-    images: [
-        'ace-youtil/ro-RO/lqvtnxzl3epkpvr8drkv',
-        'ace-youtil/ro-RO/plmiq10gf3jvtkrgokuk',
-        'ace-youtil/ro-RO/vu0hjpq185arrww5oqhz',
-        'ace-youtil/ro-RO/qymugjyrwtbnzz8dezju'
-    ]
-}
-
-MWImagePage.propTypes = {
+MWImageExample.propTypes = {
     images: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
-export default MWImagePage
+export default MWImageExample
---
+++ pages/mw-image.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import MWImage from '../components/mw-image'
+
+const images = [
+    'ace-youtil/ro-RO/lqvtnxzl3epkpvr8drkv',
+    'ace-youtil/ro-RO/plmiq10gf3jvtkrgokuk',
+    'ace-youtil/ro-RO/vu0hjpq185arrww5oqhz',
+    'ace-youtil/ro-RO/qymugjyrwtbnzz8dezju'
+]
+
+const MWImagePage = () => {
+    return (
+        <p>Hey</p>
+    )
+    return (
+        <MWImage images={images}/>
+    )
+}
+
+export default MWImagePage
