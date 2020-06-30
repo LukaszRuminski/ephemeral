6c7cb18367c4e035d9a503cfbda36959e12a5bef
Lukasz Ruminski <ruminski.lr@pg.com>
2020-06-30 18:39:50 +0200

    Centralize pipelines - replace custom server with next server

--- server.js
+++ server.js
@@ -1,5 +1,2 @@
 const cli = require('next/dist/cli/next-start')
-
-process.env.NODE_ENV = process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : 'production'
-process.env.NODE_ENV === 'production' ? process.env.DIST_DIRECTORY = '.next' : ''
-cli.nextStart(['-p', process.env.PORT || 3000])
+cli.nextStart(['-p', ${PORT:-3000}])
