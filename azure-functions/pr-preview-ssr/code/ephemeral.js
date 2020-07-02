4fdefc0d80aa865376a0dfb07fc79005f11868b5
Lukasz Ruminski <ruminski.lr@pg.com>
2020-07-02 08:17:52 +0200

    Centralize pipelines - replace custom server with next server

--- azure-functions/pr-preview-ssr/code/ephemeral.js
+++ azure-functions/pr-preview-ssr/code/ephemeral.js
@@ -1,5 +1,10 @@
-const cli = require('next/dist/cli/next-start')
+const createServer = require('next/dist/server/lib/start-server.js');
+const PORT = process.env.PORT !== undefined ? process.env.PORT : 3000;
 
-const server = cli.nextStart(['-p', process.env.PORT || 3000])
+const server = createServer.default({}, PORT).then(async app => {
+    console.log(`started server on http://localhost:${PORT}`)
+    await app.prepare()
+})
+
 
 module.exports = { appServer: server }
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const server = require('next/dist/server/lib/start-server.js');
+const createServer = require('next/dist/server/lib/start-server.js');
 const PORT = process.env.PORT !== undefined ? process.env.PORT : 3000;
 
-server.default({}, PORT).then(async app=>{// tslint:disable-next-line
-    console.log(`started server on http://localhost:${PORT}`);await app.prepare()})
+createServer.default({}, PORT).then(async app => {
+    console.log(`started server on http://localhost:${PORT}`)
+    await app.prepare()
+})
+
