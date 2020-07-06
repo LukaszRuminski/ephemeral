b4225f9ab93b3190dad6dfc6580bfe2aa130c6f0
Lukasz Ruminski <ruminski.lr@pg.com>
2020-07-06 10:35:11 +0200

    Centralize pipelines - replace custom server with next server

---
+++ azure-functions/pr-preview-ssr/handler.js
@@ -0,0 +1,62 @@
+const http = require("http")
+const isType = require('type-is')
+
+function azureFunctionHandler ({ hostname, port }) {
+
+    const getContentType = (params) => {
+        return params.contentTypeHeader ? params.contentTypeHeader.split(';')[0] : ''
+    }
+
+    const isContentTypeBinaryMimeType = (params) => {
+        return params.binaryMimeTypes.length > 0 && !!isType.is(params.contentType, params.binaryMimeTypes)
+    }
+
+    return (context, req) => {
+
+        let url = new URL(req.url)
+        let pathname = url.pathname
+
+        const requestOptions = {
+            host: hostname,
+            port: port,
+            path: pathname,
+            method: req.method,
+            headers: Object.assign({}, req.headers)
+        }
+
+        const request = http.request(requestOptions, (res) => {
+            const buffer = []
+
+            res.on('data', (chunk) => buffer.push(chunk))
+
+            res.on("end", () => {
+                const bodyBuffer = Buffer.concat(buffer)
+                const status = res.statusCode
+                const headers = res.headers
+
+                if (headers['transfer-encoding'] === 'chunked') {
+                    delete headers['transfer-encoding']
+                }
+
+                const contentType = getContentType({contentTypeHeader: headers['content-type']})
+                const isBase64Encoded = isContentTypeBinaryMimeType({contentType, binaryMimeTypes: ['*/*']})
+                const body = bodyBuffer
+
+                context.res = {status, body, headers, isBase64Encoded}
+                context.done();
+            });
+        }).on("error", (error) => {
+            context.log('error');
+            context.res = {
+                status: 500,
+                body: error
+            };
+            context.done();
+        });
+        request.end()
+    }
+}
+
+exports.default = azureFunctionHandler;
+
+module.exports = exports.default;
