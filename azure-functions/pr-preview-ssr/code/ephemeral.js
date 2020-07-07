fbd6a93f62a5b5bd97d7af5b6089b56c323dfa6b
Lukasz Ruminski <ruminski.lr@pg.com>
2020-07-07 08:07:29 +0200

    Centralize pipelines - replace custom server with next server

--- azure-functions/pr-preview-ssr/code/ephemeral.js
+++ azure-functions/pr-preview-ssr/code/ephemeral.js
@@ -1,9 +1,9 @@
 const createServer = require('next/dist/server/lib/start-server.js').default;
-const port = process.env.port || process.env.PORT || 5000
+const port = process.env.port || process.env.PORT || 80
 
 const server = createServer({dir: './code'}, port).then(async app => {
     console.log(`started server on http://localhost:${port}`)
     await app.prepare()
 })
 
-module.exports = { server, hostname: '0.0.0.0', port}
+module.exports = { server, hostname: 'localhost', port}
